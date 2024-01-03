bcc937fb0a3c0b9ae1b0333f281f2ca85c1f0ca3
archh <jules.hermyer@gmail.com>
2024-01-03 21:56:15 +0100

    og

--- main.c
+++ main.c
@@ -22,34 +22,40 @@ struct point{
 typedef struct point point;
 
 pixel get_pixel(image i, int x, int y){
+	x = (x < 0)  ? (x  + i.w) % i.w : x % i.w; 
+	y = (y < 0)  ? (y  + i.h) % i.h : y % i.h;
 	return i.pixels[i.w * y + x];
 }
 
+int rgbtohtml(pixel p){
+	return (p.r << 16) | (p.g << 8) | p.b;
+}
 
-int lightred = 0xFF8080;
-int red = 0xFF0000;
-int darkred = 0x800000;
+const int lightred = 0xFF8080;
+const int red = 0xFF0000;
+const int darkred = 0x800000;
 
-int lightyellow = 0xFFFF80;
-int yellow = 0xFFFF00;
-int darkyellow = 0x808000;
+const int lightyellow = 0xFFFF80;
+const int yellow = 0xFFFF00;
+const int darkyellow = 0x808000;
 
-int lightgreen = 0x80FF80;
-int green = 0x00FF00;
-int darkgreen = 0x008000;
+const int lightgreen = 0x80FF80;
+const int green = 0x00FF00;
+const int darkgreen = 0x008000;
 
-int lightcyan = 0x80FFFF;
-int cyan = 0x00FFFF;
-int darkcyan = 0x008080;
+const int lightcyan = 0x80FFFF;
+const int cyan = 0x00FFFF;
+const int darkcyan = 0x008080;
 
-int lightblue = 0x8080FF;
-int blue = 0x0000FF;
-int darkblue = 0x000080;
+const int lightblue = 0x8080FF;
+const int blue = 0x0000FF;
+const int darkblue = 0x000080;
 
-int lightmagenta = 0xFF80FF;
-int magenta = 0xFF00FF;
-int darkmagenta = 0x800080;
+const int lightmagenta = 0xFF80FF;
+const int magenta = 0xFF00FF;
+const int darkmagenta = 0x800080;
 
+int colortab[] = {lightred, red, darkred, lightyellow, yellow, darkyellow, lightgreen, green, darkgreen, lightcyan, cyan, darkcyan, lightblue, blue, darkblue, lightmagenta, magenta, darkmagenta};
 
 
 image read_ppm(char* filename) {
@@ -342,6 +348,93 @@ point upedge_right(image i, point start ,pixel init, int x, int y, bool* traite)
 	}
 	return p;
 }
+
+bool is_passante(pixel p){
+	return p.r * 0.202 + 0.707 * p.g + 0.071 * p.b >= 128. ;
+}
+
+bool is_block_color(pixel p){
+	int htcode = rgbtohtml(p);
+	for(int i = 0; i < 18; i++){
+		if(colortab[i] ==  htcode) return true;
+	}
+	return false;
+}
+
+
+
+point get_next_pixel_edge(image i, int x, int y, int direction, int bord){
+	point p;
+	p.x = x;
+	p.y = y;
+	bool* traite = malloc(sizeof(bool) * i.h * i.w);
+	if(direction == 1){
+		if(bord == 0) return rightedge_up(i, p, get_pixel(i, x, y), x, y, traite);
+		return rightedge_down(i, p, get_pixel(i, x, y), x, y, traite);
+	}
+	if(direction == 2){
+		if(bord == 0) return downedge_right(i, p, get_pixel(i, x, y), x, y, traite);
+		return downedge_left(i, p, get_pixel(i, x, y), x, y, traite);
+	}
+	if(direction == 3){
+		if(bord == 0) return leftedge_down(i, p, get_pixel(i, x, y), x, y, traite);
+		return leftedge_up(i, p, get_pixel(i, x, y), x, y, traite);
+	}
+	if(direction == 4) {
+		if(bord == 0) return upedge_left(i, p, get_pixel(i, x, y), x, y, traite);
+		return upedge_right(i, p, get_pixel(i, x, y), x, y, traite);
+	}
+	return rightedge_up(i, p, get_pixel(i, x, y), x, y, traite);
+}
+
+
+
+point get_next_block(image i, int x, int y,int* bord, int direction, int count){
+	int b = *bord;
+	int ox = x;
+	int oy = y;
+	int dx;
+	int dy;
+	if(direction == 0){
+		dx = 0;
+		dy = -1;
+	}
+	if(direction == 1){
+		dx = 1;
+		dy = 0;
+	}
+	if(direction == 2){
+		dx = 0;
+		dy = 1;
+	}
+	if(direction == 3){
+		dx = -1;
+		dy = 0;
+	}
+	bool check = true;
+	while(check){
+		x = x + dx;
+		y = y + dy;
+		pixel p = get_pixel(i, x, y);
+		point pp;
+		pp.x = x;
+		pp.y = y;
+		if(is_block_color(p)) return pp;
+		if(is_passante(p)) continue;
+		else{
+			if(count == 8){
+				exit(1);	
+			}
+			else{
+				*bord = (b + 2) % 4;
+				return get_next_block(i, ox, oy, bord, direction, count+1);
+			}
+		}
+	}
+}
+
+
+
 //0 nord
 //1 est
 //2 sud
@@ -349,6 +442,9 @@ point upedge_right(image i, point start ,pixel init, int x, int y, bool* traite)
 //0 babord
 //1 tribord
 void interprete(image i, int x, int y, int direction, int bord, stack* s){
+	x = (x < 0)  ? (x  + i.w) % i.w : x % i.w; 
+	y = (y < 0)  ? (y  + i.h) % i.h : y % i.h;
+	
 		
 }
 
@@ -362,9 +458,6 @@ void start(){
 }
 
 
-int rgbtohtml(pixel p){
-	return (p.r << 16) | (p.g << 8) | p.b;
-}
  
 int main() {
     char filename[] = "example.ppm";
